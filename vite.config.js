f9cc2eaaa598cea1b3b91eda0a70b8b2d64f50ae
chamsbj <chamsbj@gmail.com>
2024-10-29 22:54:27 +0100

    Update your message here

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      '/api':{
+  base: '/https://github.com/chamsbj/reactjob.git/', // Replace <your-repo-name> with your actual GitHub repository name
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite:(path)=>path.replace(/^\/api/, ''),
-      }
-    }
-
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-})
+});
